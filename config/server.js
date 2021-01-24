2bf7858bfdf73591ef405f30c30a7881bf440248
jackwilliams1998 <jackwilliams98@hotmail.com>
2021-01-24 13:25:33 +0000

    First commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd60b358760afafd7ae27381be25f3e9b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'efdc68ff-be26-4a79-a4e4-3752cb4380cf'
+};
